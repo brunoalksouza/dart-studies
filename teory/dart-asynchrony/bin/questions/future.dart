59ba0f7c7232ac5d39fd21b328481201c31c53c2
Bruno Alkmin <balkmin@grupo-artico.com>
2023-05-19 15:12:19 -0300

    test 2

---
+++ teory/dart-asynchrony/bin/questions/future.dart
@@ -0,0 +1,40 @@
+import 'dart:ffi';
+
+void main() {
+  // Future myFuture = Future(() {
+  //   print('Going back to the future');
+  //   return 21;
+  // }).then((value) => print('the value is $value'));
+
+  Future<int> myFutureFunc() async {
+    print('I have a function in the future');
+    await Future.delayed(Duration(seconds: 5));
+    throw Exception();
+    return 12;
+  }
+
+  // myFutureFunc()
+  //     .then((value) => print('My function value is $value'))
+  //     .onError((error, stackTrace) => print('An error occured'))
+  //     .whenComplete(() => print('The future is Over'));
+
+  Future<int> myFutureErrorFunc(int a, int b) async {
+    try {
+      if (a > b) {
+        throw Exception();
+      }
+
+      print('I have a functional Function');
+      await Future.delayed(Duration(seconds: 5));
+      return 42;
+    } catch (e) {
+      print('An error ocured: $e');
+      return 42;
+    } finally {
+      print('The future is finally over');
+    }
+  }
+
+  myFutureErrorFunc(2, 1).then((value) => print('the value is $value'));
+  print('Done with main');
+}
