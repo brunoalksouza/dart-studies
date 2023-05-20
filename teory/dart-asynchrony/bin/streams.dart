ae3e4e40ce1e29efc16b82c32c5a78bc7ba488e4
Bruno Souza <brunoalksouza@gmail.com>
2023-05-20 16:38:42 -0300

    finished alura course

---
+++ teory/dart-asynchrony/bin/streams.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+void main() {
+  Stream myStream(int interval, [int? maxCount]) async* {
+    int i = 1;
+    while (i != maxCount) {
+      print('Counting: $i');
+      await Future.delayed(Duration(seconds: interval));
+      yield i++;
+    }
+    print("The stream is finished");
+  }
+
+  StreamSubscription mySubscriber = myStream(1, 10).listen((event) {
+    if (event.isEven) {
+      print("This number is Even");
+    }
+  }, onError: (e) {
+    print('An error happend $e');
+  }, onDone: () {
+    print('subscriber is gone');
+  });
+  print("The main is finished");
+}
